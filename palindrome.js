e2e1caf3436a5c25b61317d7c3f0baf27b6ba38e
RajaneRahul <rahulrajane177@gmail.com>
2022-11-30 00:00:15 +0530

    its done

--- palindrome.js
+++ palindrome.js
@@ -1,8 +1,8 @@
 function checkPalindrome(N, str) {
     let bag="";
-    for(let i=str.length-1;i>=0;i++){
+    for(let i=str.length-1;i>=0;i--){
         bag=bag+str[i]
-    }if(bag==bag){
+    }if(str==bag){
         console.log("Yes")
     }else{
         console.log("No")
--- prime.js
+++ prime.js
@@ -1,11 +1,11 @@
 function identifyPrime(num) {
     let count=0;
-    for(let i=0;i<=num;i++){
+    for(let i=1;i<=num;i++){
         if(num%i==0){
             count++;
         }
     }
-    if(count=0){
+    if(count==2){
         console.log("Yes")
     }else{
         console.log("No")
